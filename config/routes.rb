91ab66ded15390bf30ff7ac6780b817737665814
Badea Cristina <badea.cristina20@yahoo.com>
2024-03-06 20:35:16 +0200

    login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   post "sign_up", to: "registration#create"
   delete "logout", to:  "sessions#destroy"
 
+  get "sign_in", to:"sessions#new"
+  post "sign_in", to: "sessions#create"
+
   # GET /
   root to:"main#index"
 end
