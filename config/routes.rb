62f60f072989b0b8bcaef126aca9f8c755ee53ea
Badea Cristina <badea.cristina20@yahoo.com>
2024-03-06 17:26:07 +0200

    logging out users

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "about-us", to:"about#index", as: :about
   get "sign_up", to:"registration#new"
   post "sign_up", to: "registration#create"
+  delete "logout", to:  "sessions#destroy"
 
   # GET /
   root to:"main#index"
