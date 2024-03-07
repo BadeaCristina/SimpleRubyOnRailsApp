4a823e73f1f7bf7ea5a859530ef2260b59817bae
Badea Cristina <badea.cristina20@yahoo.com>
2024-03-07 19:49:07 +0200

    edit password

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-  # GET /about
+  
   get "about-us", to:"about#index", as: :about
+  get "password", to:"passwords#edit", as: :edit_password
+  patch "password", to:"passwords#update" 
   get "sign_up", to:"registration#new"
   post "sign_up", to: "registration#create"
   delete "logout", to:  "sessions#destroy"
