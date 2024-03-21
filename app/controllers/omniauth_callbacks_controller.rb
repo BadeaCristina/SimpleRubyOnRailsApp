9ac70c24c633a2709342aee0599bb5c037e8bc3f
Badea Cristina <badea.cristina20@yahoo.com>
2024-03-21 17:13:42 +0200

    twitter accounts page, connect to twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,20 @@
 class OmniauthCallbacksController < ApplicationController
     def twitter
-        render plain: "Success"
+        
+        twitter_account = Current.user.twitter_accounts.where(username: auth.nickname).first_or_initialize
+        Rails.logger.info twitter_account
+        Rails.logger.info  auth.info.name
+        twitter_account.update(
+            name: auth.info.name,
+            username: auth.info.nickname,
+            image: auth.info.image,
+            token: auth.credentials.token,
+            secret: auth.credentials.secret,
+        )
+        redirect_to twitter_accounts_path, notice: "Successfuly connected your account"
+    end
+
+    def auth
+        request.env['omniauth.auth']
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
 
   get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
 
+  resources :twitter_accounts
+
+
   # GET /
   root to:"main#index"
 end
